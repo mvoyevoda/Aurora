497d122e8bf9d933387df2b3291628cfa8f2fd57
Ahmed Hamza <ahmedhamza@Ahmeds-MacBook-Air.local>
2023-07-30 23:46:07 -0400

    07/30

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Root from "./routes/root"
-import SignUp from "./routes/signup"
-import LogIn from "./routes/login"
-import "./index.css"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from "./routes/root";
+import SignUp from "./routes/signup";
+import LogIn from "./routes/login";
+import "./index.css";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 import {
   createBrowserRouter,
@@ -23,8 +28,9 @@ const router = createBrowserRouter([
   },
 ]);
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
--- client/src/routes/login.jsx
+++ client/src/routes/login.jsx
@@ -1,7 +1,80 @@
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import "./styles/signup.css"
+import Link from '@mui/material/Link';
+
+
+
+
 export default function LogIn(){
+
+    const handleLogIn = (event) => {
+        event.preventDefault();
+        var email = document.getElementById('email').value;
+        var password = document.getElementById('password').value;
+
+        console.log(email + ' ' + password);
+    }
     return (
         <>
-        <h1>Log In page</h1>
+        <h1>Log In</h1>
+        <form className="signup_form">
+            <TextField
+            sx={{
+                backgroundColor: 'rgba(217, 217, 217, 0.20)',
+                marginBottom: '10px'
+            }}
+            variant="standard"
+            label= "Email"
+            id="email"
+            fullWidth= "true"
+            required
+            />
+            <TextField
+            sx={{
+                backgroundColor: 'rgba(217, 217, 217, 0.20)',
+                marginBottom: '10px'
+            }}
+            variant="standard"
+            label= "Password"
+            id="password"
+            type="password"
+            fullWidth= "true"
+            required
+            />
+            <Link href="#" variant="p" color = 'inherit' children="Forgot password?" 
+            sx={{
+                position: 'relative',
+                left: '320px',
+                
+            }}/>
+            <Button href="./signup" 
+            variant="outlined" 
+            children= "Don't have an account?"
+            sx={{
+                marginTop: '10px',
+                color: 'white',
+                border: '1px solid',
+                borderRadius: '50px',
+                display: 'block',
+                marginBottom: '20px' 
+            }}/>
+            <Button variant="outlined"
+            type="submit" 
+            children= "Log In" 
+            onClick={handleLogIn}
+            sx={{
+                color: 'white',
+                border: '1px solid',
+                display: 'block',
+                width: '100%',
+                borderRadius: '50px',
+                '&:hover': {
+                }
+            }}/>
+
+         </form>
+
         </>
     )
 }
