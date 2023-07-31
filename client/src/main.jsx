4d16ce3a112a9e569b42d8d420d1018b52744b3e
ddamme05 <ddamme05@gmail.com>
2023-07-31 14:06:22 -0400

    Restructured to Vite.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,29 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Root from "./routes/root";
-import SignUp from "./routes/signup";
-import LogIn from "./routes/login";
-import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-  },
-  {
-    path: "/signup",
-    element: <SignUp />,
-  },
-
-  {
-    path: "/login",
-    element: <LogIn />,
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
--- client/src/routes/login.jsx
+++
@@ -1,7 +0,0 @@
-export default function LogIn(){
-    return (
-        <>
-        <h1>Log In page</h1>
-        </>
-    )
-}
