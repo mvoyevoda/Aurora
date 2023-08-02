f071912b0f4cea91cf2acb7a03f8178d1b65a333
Adefela <adefakorode@gmail.com>
2023-08-02 15:29:03 -0400

    fixed nb

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -10,14 +10,18 @@ export default function NavBar() {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" sx={{ backgroundColor: 'transparent', boxShadow: 'none' }}>
         <Toolbar sx={{ justifyContent: 'flex-start', paddingLeft: '16px' }}>
-          <Button color="inherit" sx={{ marginRight: '16px' }}>
+          <Button color="inherit" sx={{ marginRight: '16px', backgroundColor: 'rgba(212, 212, 212, 0.30)' }}>
             Home
           </Button>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             &nbsp;
           </Typography>
-          <Button color="inherit">Sign in</Button>
-          <Button color="inherit">Log in</Button>
+          <Button color="inherit" sx={{ backgroundColor: 'rgba(212, 212, 212, 0.30)' }}>
+            Log In
+          </Button>
+          <Button color="inherit" sx={{ backgroundColor: 'rgba(212, 212, 212, 0.30)', marginLeft: '30px' }}>
+            Sign in
+          </Button>
         </Toolbar>
       </AppBar>
     </Box>
