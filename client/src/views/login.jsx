e8738d0cf6aaf374c7e43964b12c7dd502ebfea4
ddamme05 <ddamme05@gmail.com>
2023-08-02 13:55:34 -0400

    Login works.

--- client/src/views/login.jsx
+++ client/src/views/login.jsx
@@ -1,24 +1,38 @@
+import { useRef } from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import "../styles/signup.css"
 import Link from '@mui/material/Link';
+import axios from 'axios';
 
 
 
 
 export default function LogIn(){
+    const emailRef = useRef();
+    const passwordRef = useRef();
 
-    const handleLogIn = (event) => {
-        event.preventDefault();
-        var email = document.getElementById('email').value;
-        var password = document.getElementById('password').value;
-
-        console.log(email + ' ' + password);
-    }
+    const handleLogIn = async (e) => {
+        e.preventDefault();
+        try {
+          const response = await axios({
+            method: 'post',
+            url: 'http://localhost:4000/api/auth/login',
+            data: {
+              email: emailRef.current.value,
+              password: passwordRef.current.value,
+            },
+            withCredentials: true,
+          });
+          console.log(response.data);
+        } catch (error) {
+          console.error(`Error: ${error}`);
+        }
+      };      
     return (
         <>
         <h1>Log In</h1>
-        <form className="signup_form">
+        <form onSubmit={ handleLogIn } className="signup_form">
             <TextField
             sx={{
                 backgroundColor: 'rgba(217, 217, 217, 0.20)',
@@ -29,6 +43,7 @@ export default function LogIn(){
             id="email"
             fullWidth= "true"
             required
+            inputRef = { emailRef }
             />
             <TextField
             sx={{
@@ -41,6 +56,7 @@ export default function LogIn(){
             type="password"
             fullWidth= "true"
             required
+            inputRef = { passwordRef }
             />
             <Link href="#" variant="p" color = 'inherit' children="Forgot password?" 
             sx={{
@@ -62,7 +78,7 @@ export default function LogIn(){
             <Button variant="outlined"
             type="submit" 
             children= "Log In" 
-            onClick={handleLogIn}
+            //onClick={handleLogIn}
             sx={{
                 color: 'white',
                 border: '1px solid',
