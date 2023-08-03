e0dc6306f3aac642f88146b0335d52eaf5f2e220
ddamme05 <ddamme05@gmail.com>
2023-08-03 12:36:16 -0400

    allowNull true.

--- server/migrations/20230803161300-add-userId-to-quizzes.js
+++ server/migrations/20230803161300-add-userId-to-quizzes.js
@@ -4,7 +4,7 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.addColumn('quizzes', 'userId', {
       type: Sequelize.INTEGER,
-      allowNull: false,
+      allowNull: true,
       references: {
         model: 'users',
         key: 'id',
