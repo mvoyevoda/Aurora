ab395bc1bd6e3da8099075a8bbbcf46cc3f7bfc2
Adefela <adefakorode@gmail.com>
2023-08-03 22:59:39 -0400

    removed SignUpButton component, it was redundant

--- client/src/views/welcome.jsx
+++ client/src/views/welcome.jsx
@@ -1,13 +1,11 @@
 import NavBar from "../components/NavBar";
 import WCard from "../components/WCard";
-import SignUpButton from "../components/SignUpButton"
 
 export default function Welcome(){
     return (
         <>
         <NavBar />
         <WCard />
-        <SignUpButton />
         </>
     )
 }
