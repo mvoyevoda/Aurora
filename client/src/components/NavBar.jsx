46b796956c849c60543e0f48676d61ebe7460d10
Adefela <adefakorode@gmail.com>
2023-08-03 22:56:09 -0400

    removed home button

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,16 +1,19 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
 export default function NavBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" sx={{ backgroundColor: 'transparent', boxShadow: 'none' }}>
-        <Toolbar sx={{ justifyContent: 'flex-start', paddingLeft: '16px' }}>
-          <Button
+      <AppBar
+        position="static"
+        sx={{ backgroundColor: "transparent", boxShadow: "none" }}
+      >
+        <Toolbar sx={{ justifyContent: "flex-start", paddingLeft: "16px" }}>
+          {/* <Button
             color="inherit"
             sx={{
               marginRight: '16px',
@@ -20,29 +23,29 @@ export default function NavBar() {
             }}
           >
             Home
-          </Button>
+          </Button> */}
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             &nbsp;
           </Typography>
           <Button
             color="inherit"
-            href='./login'
+            href="./login"
             sx={{
-              backgroundColor: 'transparent',
-              border: '1px solid white', // Add a white stroke
-              borderRadius: '4px', // Optional: add border radius to make the edges smoother
+              backgroundColor: "transparent",
+              border: "1px solid white",
+              borderRadius: "4px", 
             }}
           >
             Log In
           </Button>
           <Button
             color="inherit"
-            href='./signup'
+            href="./signup"
             sx={{
-              backgroundColor: 'transparent',
-              border: '1px solid white', // Add a white stroke
-              borderRadius: '4px', // Optional: add border radius to make the edges smoother
-              marginLeft: '30px',
+              backgroundColor: "transparent",
+              border: "1px solid white",
+              borderRadius: "4px", 
+              marginLeft: "30px",
             }}
           >
             Sign up
