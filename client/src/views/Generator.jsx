41e4fac27af2186a1457461fc494f72ccbb644ef
Ahmed Hamza <ahmedhamza@Ahmeds-MacBook-Air.local>
2023-08-06 20:10:07 -0400

    added nav button to dashboard, and logout

--- client/src/views/Generator.jsx
+++ client/src/views/Generator.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Configurator from "../components/Configurator";
 import NavDashboard from "../components/Dashboard Section/NavDashboard";
+import LogoutButton from "../components/Dashboard Section/LogoutButton";
 
 const styles = {
   container: {
@@ -8,19 +9,22 @@ const styles = {
     minHeight: "100vh",
   },
   content: {
-    paddingBottom: "9vh", // Adjust the padding to make space for the NavDashboard button
+    //marginBottom: "1rem", // Adjust the padding to make space for the NavDashboard button
   },
   navDashboard: {
     position: "absolute",
     bottom: 0,
     left: 0,
     width: "100%",
-    height: "19vh",
+    height: "4rem",
+    marginBottom: "4.3rem",
   },
 };
 
 function Generator() {
   return (
+    <>
+    <LogoutButton />
     <div style={styles.container}>
       <div style={styles.content}>
         <Configurator />
@@ -29,6 +33,7 @@ function Generator() {
         <NavDashboard />
       </div>
     </div>
+    </>
   );
 }
 
