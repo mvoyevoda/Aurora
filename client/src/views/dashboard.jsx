13e942d5c118fb22a9d6553bb7f798d2992f7306
Adefela <adefakorode@gmail.com>
2023-08-07 11:09:57 -0400

    currently adding container/column for recent quizzes side

--- client/src/views/dashboard.jsx
+++ client/src/views/dashboard.jsx
@@ -1,11 +1,12 @@
+//import LogoutButton from "../components/Dashboard Section/LogoutButton"
 import NavGenerator from "../components/Dashboard Section/NavGenerator";
-import { QuizGen } from "../components/Dashboard Section/QuizGen";
+import {QuizGen} from "../components/Dashboard Section/QuizGen";
 import FavCategory from "../components/Dashboard Section/FavCategory";
 import RecentTitle from "../components/Dashboard Section/RecentQuizzes/RecentTitle";
+import "../styles/dashboard.css"
 import SuggestedTitle from "../components/Dashboard Section/SuggestedQuizzes/SuggestedTitle";
-import NavSettings from "../components/Dashboard Section/NavSettings";
-import React, { useState, useEffect } from "react";
-import Button from '@mui/material/Button';
+import NavSettings from "../components/Dashboard Section/NavSettings"
+
 
 const styles = {
   pcContainer: {
@@ -23,48 +24,19 @@ const styles = {
     display: 'flex',
     flexDirection: 'row',
   },
+  recentColumn:{
+    flex: 2,
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  suggestedColumn:{
+    flex: 2,
+    display: 'flex',
+    flexDirection: 'column',
+  }
 };
 
 export default function Dashboard() {
-  const [recentQuizzes, setRecentQuizzes] = useState([]); // Array to hold recent quizzes data
-  const [suggestedQuizzes, setSuggestedQuizzes] = useState([]); // Array to hold suggested quizzes data
-  const [recentQuizzesCount, setRecentQuizzesCount] = useState(0); // Number of recent quizzes displayed
-  const [suggestedQuizzesCount, setSuggestedQuizzesCount] = useState(0); // Number of suggested quizzes displayed
-  const totalRecentQuizzes = 50; // Total number of recent quizzes available
-  const totalSuggestedQuizzes = 50; // Total number of suggested quizzes available
-  const loadMoreCount = 10; // Number of quizzes to load on each "Load More" click
-
-  useEffect(() => {
-    // Fetch recent quizzes and suggested quizzes and store them in state
-    // You can use an API call or any other data source here.
-    const recentQuizzesData = getRecentQuizzes(); // Implement this function to fetch recent quizzes.
-    setRecentQuizzes(recentQuizzesData);
-    const suggestedQuizzesData = getSuggestedQuizzes(); // Implement this function to fetch suggested quizzes.
-    setSuggestedQuizzes(suggestedQuizzesData);
-  }, []);
-
-  // Example function to get recent quizzes from an API.
-  async function getRecentQuizzes() {
-    const response = await fetch('your_api_endpoint_for_recent_quizzes');
-    const data = await response.json();
-    return data;
-  }
-
-  // Example function to get suggested quizzes from an API.
-  async function getSuggestedQuizzes() {
-    const response = await fetch('your_api_endpoint_for_suggested_quizzes');
-    const data = await response.json();
-    return data;
-  }
-
-  const handleLoadMoreRecent = () => {
-    setRecentQuizzesCount(prevCount => prevCount + loadMoreCount);
-  };
-
-  const handleLoadMoreSuggested = () => {
-    setSuggestedQuizzesCount(prevCount => prevCount + loadMoreCount);
-  };
-
   return (
     <>
       <NavGenerator />
@@ -74,31 +46,15 @@ export default function Dashboard() {
           <FavCategory />
         </div>
         <div style={styles.rightColumn}>
+          <div style ={styles.recentColumn}>
           <RecentTitle />
-          {recentQuizzes.slice(0, recentQuizzesCount).map((quiz, index) => (
-            <div key={index}>
-              {/* Render the recent quizzes here */}
-              <p>{quiz.title}</p>
-            </div>
-          ))}
-          {recentQuizzesCount < totalRecentQuizzes && (
-            <Button onClick={handleLoadMoreRecent}>Load More</Button>
-          )}
-        </div>
-        <div style={styles.rightColumn}>
+          </div>
+          <div style={styles.suggestedColumn}>
           <SuggestedTitle />
-          {suggestedQuizzes.slice(0, suggestedQuizzesCount).map((quiz, index) => (
-            <div key={index}>
-              {/* Render the suggested quizzes here */}
-              <p>{quiz.title}</p>
-            </div>
-          ))}
-          {suggestedQuizzesCount < totalSuggestedQuizzes && (
-            <Button onClick={handleLoadMoreSuggested}>Load More</Button>
-          )}
+          </div>
         </div>
-      </div>
-      <NavSettings />
+    </div>    
+    <NavSettings/>
     </>
   );
 }
