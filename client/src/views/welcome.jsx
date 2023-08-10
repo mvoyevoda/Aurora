a464552e238ed50049a5f340aee34a7715754037
Adefela <adefakorode@gmail.com>
2023-08-09 22:20:10 -0400

    added back protected route to dashboard

--- client/src/views/welcome.jsx
+++ client/src/views/welcome.jsx
@@ -1,9 +1,10 @@
 import NavBar from "../components/NavBar";
 import WCard from "../components/WCard";
+//need to add overflowY: 'scroll' into styling
 
 export default function Welcome() {
   return (
-    <div style={{ height: "100vh", overflowY: 'scroll'}}>
+    <div style={{ height: "100vh"}}>
       <NavBar />
       <WCard />
     </div>
