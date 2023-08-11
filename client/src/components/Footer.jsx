af7b288fc23e3daef6bb2b11e0d83c92c482ca42
Adefela <adefakorode@gmail.com>
2023-08-11 10:57:10 -0400

    created footer component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+
+export default function Footer() {
+  return (
+    <Card sx={{ 
+    minWidth: 275,
+    marginTop: '42px',
+    backgroundColor: 'transparent',
+    boxShadow: 'none',
+    }}>
+      <CardContent>
+        <Typography sx={{fontSize: 14}} marginTop= "5px" color="rgba(212, 212, 212, 1)" gutterBottom>
+        © 2023 Aurora. All rights reserved.
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
