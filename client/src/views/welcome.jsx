ed7cb8768144434dadd732f6102b8496f6feb5eb
Adefela <adefakorode@gmail.com>
2023-08-11 10:58:06 -0400

    added footer to landing page

--- client/src/views/welcome.jsx
+++ client/src/views/welcome.jsx
@@ -1,12 +1,14 @@
 import NavBar from "../components/NavBar";
 import WCard from "../components/WCard";
 //need to add overflowY: 'scroll' into styling
+import Footer from "../components/Footer";
 
 export default function Welcome() {
   return (
     <div style={{ height: "100vh"}}>
       <NavBar />
       <WCard />
+      <Footer />
     </div>
   );
 }
