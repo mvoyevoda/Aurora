2a4cefae715d941fb946fd6cc3d30f16b55c286f
Maxim Voyevoda <maximvoyevoda@gmail.com>
2023-08-16 19:13:54 -0400

    organized file structure

--- client/src/views/dashboard.jsx
+++ client/src/views/dashboard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useMediaQuery, useTheme } from "@mui/material"; 
 import NavGenerator from "../components/Dashboard Section/NavGenerator";
-import {QuizGen} from "../components/Dashboard Section/QuizGen";
+import { QuizGen } from "../components/Dashboard Section/QuizGen";
 import FavCategory from "../components/Dashboard Section/FavCategory";
 import RecentTitle from "../components/Dashboard Section/RecentQuizzes/RecentTitle";
 import RecentQuizzesList from "../components/Dashboard Section/RecentQuizzes/RecentQuizzesList";
