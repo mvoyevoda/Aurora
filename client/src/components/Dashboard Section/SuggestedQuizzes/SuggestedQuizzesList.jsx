02decaa13a1ce13199e4eb464cccb2f7f502a2ce
ahmed <hamzaa667788@gmail.com>
2023-08-16 15:31:46 -0400

    replaced suggested with popular

--- client/src/components/Dashboard Section/SuggestedQuizzes/SuggestedQuizzesList.jsx
+++ client/src/components/Dashboard Section/SuggestedQuizzes/SuggestedQuizzesList.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
+import axios from "axios";
 import { useMediaQuery, useTheme } from "@mui/material";
 
 
@@ -33,6 +35,20 @@ const SuggestedQuizzesList = () => {
 
   const itemsPerPage = isMobile ? 5 : 8; 
   const [currentPage, setCurrentPage] = useState(1);
+  const [recentQuizzes, setRecentQuizzes] = useState([]);
+
+  useEffect(() => {
+    // Fetch recent quizzes from your backend API
+    axios
+      .get("/api/quizzes/") 
+      .then((response) => {
+        setRecentQuizzes(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+  }, []);
 
   const handleShowMore = () => {
     setCurrentPage(prevPage => prevPage + 1);
@@ -44,52 +60,55 @@ const SuggestedQuizzesList = () => {
 
   const startIndex = (currentPage - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
+  const shouldEnableScroll = recentQuizzes.length > itemsPerPage;
 
 
   return (
-    <div>
+    <div style={{ maxHeight: "690px", overflowY: shouldEnableScroll ? "auto" : "initial" }}>
       <div>
-        {mockRecentQuizzes.slice(startIndex, endIndex).map((quizTitle, index) => (
-          <Card
-            key={index}
-            variant="outlined"
-            style={{
-              marginBottom: "10px",
-              backgroundColor: "rgba(212, 212, 212, .05)",
-              color: "white",
-              borderRadius: '20px',
-              width: "40vh",
-              height: "7vh",
-              marginLeft: "47px",
-              ...(isMobile && {
-                width: "20vh", // Apply this style for small screens
-              }),
-              
-            }}
-          >
-            <CardContent>
-              <Typography
-                variant="h6"
-                component="h2"
-                style={{ fontSize: "30px", color:"rgba(255, 255, 255, .9)",
-                whiteSpace: "nowrap", // Prevent text from wrapping
-                  overflow: "hidden",   // Hide overflowing text
-                  textOverflow: "ellipsis", // Display ellipsis (...) for overflow
-                  transition: "all 0.3s", // Add smooth transition effect
-                  ":hover": {
-                    whiteSpace: "normal", // Display full text on hover
-                    overflow: "visible",  // Show all text
-                    textOverflow: "unset", // Remove ellipsis
-                    fontSize: "1.3em", // Increase font size on hover
-                  },
-                  ...(isMobile && {
-                fontSize: "1.3em", // Apply this style for small screens
-              }),
-            }}>
-                {quizTitle}
-              </Typography>
-            </CardContent>
-          </Card>
+        {recentQuizzes.slice(startIndex, endIndex).map((quiz, index) => (
+          <Link to={`/portal/${quiz.id}`} key={quiz.id} style={{ textDecoration: "none" }}>
+            <Card
+              key={index}
+              variant="outlined"
+              style={{
+                marginBottom: "10px",
+                backgroundColor: "rgba(212, 212, 212, .05)",
+                color: "white",
+                borderRadius: '20px',
+                width: "40vh",
+                height: "7vh",
+                marginLeft: "47px",
+                ...(isMobile && {
+                  width: "20vh", // Apply this style for small screens
+                }),
+                
+              }}
+            >
+              <CardContent>
+                <Typography
+                  variant="h6"
+                  component="h2"
+                  style={{ fontSize: "30px", color:"rgba(255, 255, 255, .9)",
+                  whiteSpace: "nowrap", // Prevent text from wrapping
+                    overflow: "hidden",   // Hide overflowing text
+                    textOverflow: "ellipsis", // Display ellipsis (...) for overflow
+                    transition: "all 0.3s", // Add smooth transition effect
+                    ":hover": {
+                      whiteSpace: "normal", // Display full text on hover
+                      overflow: "visible",  // Show all text
+                      textOverflow: "unset", // Remove ellipsis
+                      fontSize: "1.3em", // Increase font size on hover
+                    },
+                    ...(isMobile && {
+                  fontSize: "1.3em", // Apply this style for small screens
+                }),
+              }}>
+                  {quiz.category}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Link>
         ))}
       </div>
       {currentPage > 1 && (
